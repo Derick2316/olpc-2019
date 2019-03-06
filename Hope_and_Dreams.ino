6a6a977e7b136e2cec8a326429589b197edc4b0a
sno-oze <41127531+sno-oze@users.noreply.github.com>
2019-03-06 13:25:43 +0700

    Add files via upload

---
+++ Hope_and_Dreams.ino
@@ -0,0 +1,73 @@
+#include <Servo.h>
+#include <PS2X_lib.h>
+#define enA 9 // Left
+#define enB 10 // Right
+#define in1 4
+#define in2 5
+#define in3 6
+#define in4 7
+#define PS2_DAT 13 // data
+#define PS2_CMD 11 //command
+#define PS2_SEL 10 // attention
+#define PS2_CLK 12 //clock
+
+PS2X ps2x;
+int xAxis, yAxis, x = 0, y = 0, motorSpeedA = 0,  motorSpeedB = 0;
+
+void setup() {
+  ps2x.config_gamepad
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  xAxis = yAxis = 510; 
+  while (Serial.available() == 0) {}
+  x = Serial.read(); delay(10);
+  y = Serial.read(); delay(10);
+  xAxis = x * 4; yAxis = y * 4;
+  if (yAxis < 470) { // Move Backward
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW); 
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    motorSpeedA = map(yAxis, 470, 0, 0, 255);
+    motorSpeedB = map(yAxis, 470, 0, 0, 255);
+  }
+  else if (yAxis > 550) { // Move Forward
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    motorSpeedA = map(yAxis, 550, 1023, 0, 255);
+    motorSpeedB = map(yAxis, 550, 1023, 0, 255);
+  }
+  else { // Stop
+    motorSpeedA = 0;
+    motorSpeedB = 0;
+  }
+  if (xAxis < 470) { // Left Control
+    int xMapped = map(xAxis, 470, 0, 0, 255);
+    motorSpeedA -= xMapped;
+    motorSpeedB += xMapped;
+    motorSpeedA = max(motorSpeedA, 0);
+    motorSpeedB = min(motorSpeedB, 255);
+  }
+  if (xAxis > 550) { // Right Control
+    int xMapped = map(xAxis, 550, 1023, 0, 255);
+    motorSpeedA += xMapped;
+    motorSpeedB -= xMapped;
+    motorSpeedA = min(mototSpeedA, 255);
+    motorSpeedB = max(mototSpeedB, 0);
+  }
+  // Buzz Fix
+  //if (motorSpeedA < 70) motorSpeedA = 0;
+  //if (motorSpeedB < 70) motorSpeedB = 0;
+  // Maneuver
+  analogWrite(enA, motorSpeedA); 
+  analogWrite(enB, motorSpeedB); 
+}
